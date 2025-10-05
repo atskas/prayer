6889a7bab6cb40c5b8fc8d9793be2f17c5fb04eb
atskas <atskas0@gmail.com>
2025-10-05 19:06:35 +0300

    Heavily clean up build script, remove unnecessary `CMakeLists.txt`

--- build.sh
+++ build.sh
@@ -5,42 +5,25 @@
 set -e # stops on errors
 
 mkdir -p build
-mkdir -p build/shell
-mkdir -p build/shell/commands
 
 # assemble asm files
-nasm -f elf32 kernel/asm/gdt.asm -o build/gdt.o
-nasm -f elf32 kernel/asm/boot.asm -o build/boot.o
-nasm -f elf32 kernel/asm/inter_asm.asm -o build/inter_asm.o
-nasm -f elf32 kernel/asm/exc_asm.asm -o build/exc_asm.o
-nasm -f elf32 kernel/asm/pag_asm.asm -o build/pag_asm.o
+find kernel/asm -name "*.asm" | while read -r file; do
+    out="build/$(basename "${file%.*}").o"
+    nasm -f elf32 "$file" -o "$out"
+done
 
-# file compilation
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/kmain.cpp -o build/kmain.o # kmain.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/panic.cpp -o build/panic.o # panic.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/vga.cpp -o build/vga.o # vga.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/inter.cpp -o build/inter.o # inter.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/exc.cpp -o build/exc.o # exc.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/keyboard.cpp -o build/keyboard.o # keyboard.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/pag.cpp -o build/pag.o # pag.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/shell.cpp -o build/shell/shell.o # shell.cpp
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/time.cpp -o build/time.o # time.cpp
+CFLAGS="-ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions"
 
-# commands
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/echo.cpp -o build/shell/commands/echo.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/banner.cpp -o build/shell/commands/banner.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/clear.cpp -o build/shell/commands/clear.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/color.cpp -o build/shell/commands/color.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/help.cpp -o build/shell/commands/help.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/fillc.cpp -o build/shell/commands/fillc.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/uptime.cpp -o build/shell/commands/uptime.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/shutdown.cpp -o build/shell/commands/shutdown.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/reboot.cpp -o build/shell/commands/reboot.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/math.cpp -o build/shell/commands/math.o
-g++ -ffreestanding -m32 -fno-stack-protector -fno-rtti -fno-exceptions -c kernel/shell/commands/pray.cpp -o build/shell/commands/pray.o
+# assemble cpp files
+find kernel -name "*.cpp" | while read -r file; do
+    out="build/${file%.*}.o"
+    mkdir -p "$(dirname "$out")"
+    g++ $CFLAGS -c "$file" -o "$out"
+done
 
 # link everything
-ld -m elf_i386 -n -T linker.ld -o build/kernel.elf build/gdt.o build/boot.o build/inter_asm.o build/exc_asm.o build/pag_asm.o build/kmain.o build/panic.o build/vga.o build/inter.o build/exc.o build/keyboard.o build/pag.o build/shell/shell.o build/shell/commands/echo.o build/shell/commands/banner.o build/shell/commands/clear.o build/shell/commands/color.o build/shell/commands/help.o build/shell/commands/fillc.o build/time.o build/shell/commands/uptime.o build/shell/commands/shutdown.o build/shell/commands/reboot.o build/shell/commands/math.o build/shell/commands/pray.o
+mapfile -t objs < <(find build -type f -name "*.o" | sort)
+ld -m elf_i386 -n -T linker.ld -o build/kernel.elf "${objs[@]}"
 
 # move the .elf
 mv build/kernel.elf isodir/boot/kernel.elf
